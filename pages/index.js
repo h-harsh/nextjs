917e2b04afbc57f6a91b06251a9a63a8c7aaa351
Harsh Porwal <porwalharshp@gmail.com>
2022-10-15 15:30:30 +0530

    test from zsh terminal

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Helo moto!</a>
+          Welcome to <a href="https://nextjs.org">bye moto!</a>
         </h1>
 
         <p className={styles.description}>
